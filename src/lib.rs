d4573b2853eb78cd23510c9d3056a9bcff484137
johnstclair <167724101+johnstclair@users.noreply.github.com>
2025-03-17 15:07:10 -0700

    things kinda working

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,116 @@
-pub fn point_to_buffer(mut x: f32, mut y: f32, rows: usize, cols: usize) -> Option<usize> {
+pub fn point_to_buffer(x: f32, y: f32, rows: usize, cols: usize) -> Option<usize> {
     if x <= 0.0 || y <= 0.0 || x >= rows as f32 || y >= cols as f32 {
         return None;
     }
     let (x, y): (usize, usize) = (x.floor() as usize, y.floor() as usize);
     Some(y * rows + x)
+}
+
+pub fn buffer_to_point(p: usize, rows: usize, cols: usize) -> (usize, usize) {
+    (p % cols, p / rows)
+}
+
+pub enum Id {
+    Empty,
+    Sand,
+    Water,
+}
+
+pub struct World {
+    rows: usize,
+    cols: usize,
+    buffer: Vec<Box<dyn Pixel>>,
+}
+
+impl World {
+    pub fn new(rows: usize, cols: usize) -> World {
+        let mut buffer: Vec<Box<dyn Pixel>> = Vec::new();
+        for r in 0..rows {
+            for c in 0..cols {
+                buffer.push(Box::new(Empty::new((r, c))))
+            }
+        }
+
+        World { rows, cols, buffer }
+    }
+
+    pub fn id_lize(&self) {
+        for i in &self.buffer {
+            match i.id() {
+                Id::Empty => println!("empty {:?}", i.get_pos()),
+                _ => println!("not emptry"),
+            }
+        }
+    }
+
+    fn create_pixel(id: Id, pos: (usize, usize)) -> Box<dyn Pixel> {
+        match id {
+            Id::Empty => Box::new(Empty::new(pos)),
+            Id::Sand => Box::new(Sand::new(pos)),
+            Id::Water => Box::new(Sand::new(pos)),
+        }
+    }
+
+    pub fn change_pixel(&mut self, p: usize, id: Id) {
+        self.buffer[p] = Self::create_pixel(id, buffer_to_point(p, self.rows, self.cols));
+    }
+}
+
+pub trait Pixel {
+    fn update(&self) -> Option<(usize, usize)>;
+    fn id(&self) -> Id;
+    fn set_pos(self, pos: (usize, usize)) -> (usize, usize);
+    fn get_pos(&self) -> (usize, usize);
+}
+
+pub struct Empty {
+    pos: (usize, usize),
+}
+
+impl Empty {
+    pub fn new(pos: (usize, usize)) -> Empty {
+        Empty { pos }
+    }
+}
+
+impl Pixel for Empty {
+    fn update(&self) -> Option<(usize, usize)> {
+        None
+    }
+    fn id(&self) -> Id {
+        Id::Empty
+    }
+    fn set_pos(mut self, pos: (usize, usize)) -> (usize, usize) {
+        self.pos = pos;
+        pos
+    }
+    fn get_pos(&self) -> (usize, usize) {
+        self.pos
+    }
+}
+
+pub struct Sand {
+    pos: (usize, usize),
+}
+
+impl Sand {
+    pub fn new(pos: (usize, usize)) -> Sand {
+        Sand { pos }
+    }
+}
+
+impl Pixel for Sand {
+    fn update(&self) -> Option<(usize, usize)> {
+        Some((12, 12))
+    }
+    fn id(&self) -> Id {
+        Id::Sand
+    }
+    fn set_pos(mut self, pos: (usize, usize)) -> (usize, usize) {
+        self.pos = pos;
+        pos
+    }
+    fn get_pos(&self) -> (usize, usize) {
+        self.pos
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use minifb::{Key, MouseButton, MouseMode, Scale, ScaleMode, Window, WindowOptions};
-use rand::Rng;
-use std::{thread, time::Duration};
+// use std::{thread, time::Duration};
 
 use lfisiks;
 
-const WIDTH: usize = 30;
+const WIDTH: usize = 10;
 const HEIGHT: usize = 30;
 
 fn main() {
@@ -27,28 +26,36 @@ fn main() {
     // Limit to max ~60 fps update rate
     window.set_target_fps(60);
 
-    while window.is_open() && !window.is_key_down(Key::Escape) {
-        let mut black = true;
-        for i in buffer.iter_mut() {
-            if black {
-                *i = 0x00000000;
-            } else {
-                *i = 0x00FF00FF;
-            }
-            black = !black;
-        }
+    let world = lfisiks::World::new(WIDTH, HEIGHT);
+    world.id_lize();
 
+    const SAND: u32 = 0x00ffc433;
+    const EMPTY: u32 = 0x00000000;
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
         if let Some((x, y)) = window.get_mouse_pos(MouseMode::Discard) {
             if let Some(p) = lfisiks::point_to_buffer(x, y, WIDTH, HEIGHT) {
-                buffer[p] = 0x00FFFFFF;
+                if window.get_mouse_down(MouseButton::Left) {
+                    buffer[p] = SAND;
+                }
+
+                if window.get_mouse_down(MouseButton::Right) {
+                    buffer[p] = EMPTY;
+                }
             }
         }
 
-        if let Some((scroll_x, scroll_y)) = window.get_scroll_wheel() {
-            println!("Scrolling {} - {}", scroll_x, scroll_y);
+        for x in 0..WIDTH + 32 {
+            if let Some(p) = lfisiks::point_to_buffer(x as f32, x as f32, WIDTH, HEIGHT) {
+                buffer[p] = 0x0000FF00;
+            }
         }
 
-        buffer[500] = 0x0000FF00;
+        // let mut p: usize = WIDTH * HEIGHT;
+        for i in buffer.iter_mut().rev() {
+            // p -= 1;
+            if *i == SAND {}
+        }
 
         // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
         window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
