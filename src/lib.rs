c6ca9d600f7110bd44209cebc1b233d7d252ffee
johnstclair <167724101+johnstclair@users.noreply.github.com>
2025-03-19 16:43:39 -0700

    revert: back to old water flow

--- src/lib.rs
+++ src/lib.rs
@@ -152,29 +152,6 @@ impl World {
         None
     }
 
-    pub fn check_direction_if_equal(
-        &self,
-        density: u32,
-        pos: (usize, usize),
-        dir: (isize, isize),
-    ) -> Option<(usize, usize)> {
-        if pos.0 as isize + dir.0 < 0 {
-            return None;
-        }
-        if let Some(target) = self.get_id_of((
-            (pos.0 as isize + dir.0).max(0) as usize,
-            (pos.1 as isize + dir.1).max(0) as usize,
-        )) {
-            if density == Id::density(target) {
-                return Some((
-                    (pos.0 as isize + dir.0).max(0) as usize,
-                    (pos.1 as isize + dir.1).max(0) as usize,
-                ));
-            }
-        }
-        None
-    }
-
     pub fn id_lize(&self) {
         let mut counter = 1;
         let mut e = 0;
@@ -333,17 +310,6 @@ impl Pixel for Water {
             if let Some(target) = world.check_direction(Id::density(self.id()), self.pos, (-1, 1)) {
                 return Some((target.0, target.1));
             }
-            if let Some(_target) =
-                world.check_direction_if_equal(Id::density(self.id()), self.pos, (0, -1))
-            {
-                return Some((self.pos.0 - 1, self.pos.1));
-            }
-            if let Some(_target) =
-                world.check_direction_if_equal(Id::density(self.id()), self.pos, (1, 0))
-            {
-                return Some((self.pos.0 - 1, self.pos.1));
-            }
-            let mut offset = -1;
             let mut offset = -1;
             while let Some(_target) =
                 world.check_direction(Id::density(self.id()) + 1, self.pos, (offset, 0))
@@ -359,16 +325,6 @@ impl Pixel for Water {
         if let Some(_target) = world.check_direction(Id::density(self.id()), self.pos, (1, 0)) {
             if let Some(target) = world.check_direction(Id::density(self.id()), self.pos, (1, 1)) {
                 return Some((target.0, target.1));
-            }
-            if let Some(_target) =
-                world.check_direction_if_equal(Id::density(self.id()), self.pos, (0, -1))
-            {
-                return Some((self.pos.0 + 1, self.pos.1));
-            }
-            if let Some(_target) =
-                world.check_direction_if_equal(Id::density(self.id()), self.pos, (-1, 0))
-            {
-                return Some((self.pos.0 + 1, self.pos.1));
             }
             let mut offset = 2;
             while let Some(_target) =
